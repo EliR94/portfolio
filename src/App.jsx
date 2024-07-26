0a56ac12bbc7eacfc28f50ad92851db1545f5f6f
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2024-07-26 17:07:29 +0100

    improves light mode styling throughout, adds buttons to see more about project and adds routing for project pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import NotFound from './components/NotFound';
 import ProjectsList from './components/ProjectsList'
+import ProjectItem from './components/ProjectItem';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<ProjectsList />} />
+          <Route path='/projects/:project' element={<ProjectItem />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./NavBar"
 import data from '../data.json'
+import { Link } from "react-router-dom";
 
 export default function Projects() {
     return (
@@ -12,8 +13,11 @@ export default function Projects() {
                     <li key={project.title} className="projectCard">
                         <h3>{project.title}</h3>
                         {project.image_name ? 
-                        <img className="projectImage" src={`src/assets/${project.image_name}`} alt={`${project.title} preview image`}/>
+                        <img className="projectImageCropped" src={`src/assets/${project.image_name}`} alt={`${project.title} preview image`}/>
                         : null}
+                        <button type="button" className="seeMoreButton">
+                            <Link className="seeMoreLink" to={`/projects/${project.title.replaceAll(' ', '-')}`}><p className="seeMoreText">See more!</p></Link>
+                        </button>
                     </li>
                     )}
                 </ul>
